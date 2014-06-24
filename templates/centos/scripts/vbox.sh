d24f809a353b1037447fe6fdcfa6fc0aa6e06282
Rafael Casado Sanchez <rafacas@gmail.com>
2014-06-24 15:57:39 +0200

    Added comments

--- templates/centos/scripts/vbox.sh
+++ templates/centos/scripts/vbox.sh
@@ -1,5 +1,8 @@
-echo "packer builder type: $PACKER_BUILDER_TYPE"
+#!/bin/bash -eux
+
 if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
+    echo "Installing VirtualBox guest additions"
+
     # Assume that we've installed all the prerequisites:
     # kernel-headers-$(uname -r) kernel-devel-$(uname -r) gcc make perl
     # from the install media via ks.cfg

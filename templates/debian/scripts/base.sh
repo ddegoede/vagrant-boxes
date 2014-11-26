5e9296f6682d30093850ad9b41fc5c3a42bce64a
Rafael Casado Sanchez <rafacas@gmail.com>
2014-11-26 23:36:26 +0100

    Have to use expect to 'press' q when installing wget because it shows a message even running apt-get install with the -y option

--- templates/debian/scripts/base.sh
+++ templates/debian/scripts/base.sh
@@ -4,5 +4,6 @@ echo "Disabling CDROM entries to avoid prompts to insert a disk"
 sed -i "/^deb cdrom:/s/^/#/" /etc/apt/sources.list
 
 apt-get -y update
-apt-get -y install curl wget
+apt-get -y install curl expect
+/usr/bin/expect -c 'expect "quit)" { eval spawn apt-get -y install wget; send "q\n" }'
 apt-get clean
